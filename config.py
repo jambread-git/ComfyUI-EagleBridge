2a9c0ead9167965ad0cf065a037e9250314fbbad
Jambread <265747970+jambread-git@users.noreply.github.com>
2026-09-22 20:16:07 +0900

    Disable Eagle API timeouts

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 EAGLE_HOST = "127.0.0.1"
 EAGLE_PORT = 41595
 
-EAGLE_LIST_TIMEOUT = 10
-EAGLE_ADD_TIMEOUT = 30
+# None disables Requests' connect/read timeout. Eagle API calls will wait
+# until Eagle responds or the underlying connection is terminated.
+EAGLE_LIST_TIMEOUT = None
+EAGLE_ADD_TIMEOUT = None
 
 LIBRARY_ROOT_OPTION = "[Library Root]"
 EAGLE_OFFLINE_OPTION = "[Eagle is not running]"
